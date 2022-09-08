68cd37d7d709b122149c3f356c8a3654a9b12400
Valéria <valeriamenezes022@gmail.com>
2022-09-08 18:23:16 -0300

    adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import * as api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Project
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          {api.getCategories().then((categories) => { console.log(categories); })}
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
